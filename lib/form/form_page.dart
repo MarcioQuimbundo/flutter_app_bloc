cea882f3b671e3e276dab213fd814cc9f3499044
JohnKuan <founders2008@hotmail.com>
2019-01-22 17:00:07 +0800

    added form page
    Signed-off-by: JohnKuan <founders2008@hotmail.com>

---
+++ lib/form/form_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class FormPage extends StatelessWidget {
+  Widget _buildPage({int index, Color color}) {
+    return Container(
+      alignment: AlignmentDirectional.center,
+      color: color,
+      child: Text(
+        '$index',
+        style: TextStyle(fontSize: 132.0, color: Colors.white),
+      ),
+    );
+  }
+
+  Widget _buildPageView() {
+    return PageView(
+      children: [
+        _buildPage(index: 1, color: Colors.green),
+        _buildPage(index: 2, color: Colors.blue),
+        _buildPage(index: 3, color: Colors.indigo),
+        _buildPage(index: 4, color: Colors.red),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildPageView(),
+    );
+  }
+}
