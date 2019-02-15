902b0cd886fab036f6dddcd1bf2fa69a2ae4ca0a
JohnKuan <founders2008@hotmail.com>
2019-02-15 14:23:54 +0800

    change the defaultHome when it has been authorized

--- lib/main.dart
+++ lib/main.dart
@@ -26,21 +26,25 @@ void main() {
 }
 
 class App extends StatefulWidget {
+  final UserRepository userRepository = UserRepository();
+  Widget defaultHome;
+
   @override
   State<App> createState() => AppState();
 }
 
 class AppState extends State<App> {
   AuthenticationBloc _authenticationBloc;
-  final UserRepository _userRepository = UserRepository();
 
 //  final ListRepository _listRepository = ListRepository(
 //      listApiProvider: MovieListApiProvider("http://api.themoviedb.org"));
 
   @override
   void initState() {
-    _authenticationBloc = AuthenticationBloc(userRepository: _userRepository);
+    _authenticationBloc = AuthenticationBloc(userRepository: widget.userRepository);
     _authenticationBloc.dispatch(AppStart());
+    widget.defaultHome = LoginPage(userRepository: widget.userRepository);
+
     super.initState();
   }
 
@@ -54,52 +58,43 @@ class AppState extends State<App> {
   Widget build(BuildContext context) {
     return BlocProvider<AuthenticationBloc>(
         bloc: _authenticationBloc,
-        child: MaterialApp(routes: <String, WidgetBuilder>{
-          "/": (BuildContext context) {
-            return BlocBuilder<AuthenticationEvent, AuthenticationState>(
-                bloc: _authenticationBloc,
-                builder: (BuildContext context, AuthenticationState state) {
-                  List<Widget> widgets = [];
-                  if (state.isAuthenticated) {
-                    widgets.add(HomePage());
-                  } else {
-                    widgets.add(LoginPage(
-                      userRepository: _userRepository,
-                    ));
-                  }
-                  return Stack(
-                    children: widgets,
-                  );
-                });
-          },
-          "/main": (BuildContext context) => new HomePage(),
-          "/movie": (BuildContext context) =>
-          new ListPage(
-              title: "Movies",
-              userRepository: _userRepository,
-              listRepository: ListRepository(
-                  listApiProvider:
-                  MovieListApiProvider("http://api.themoviedb.org"),
-                  listType: SupportedListItems.movie)),
-          "/equipment": (BuildContext context) =>
-          new ListPage(
-              title: "Equipments",
-              userRepository: _userRepository,
-              listRepository: ListRepository(
-                  listApiProvider:
-                  EquipmentListApiProvider(
-                      "https://9134d485-86e7-4fd7-afd5-28500eb1c97d.mock.pstmn.io"),
-                  listType: SupportedListItems.equipments)),
-          "/scan": (BuildContext context) => new ScanScreen(),
-          "/note": (BuildContext context) => new FormPage(),
-          "/activity": (BuildContext context) =>
-          new ListPage(
-              title: "Activities",
-              userRepository: _userRepository,
-              listRepository: ListRepository(
-                  listApiProvider: ActivityListApiProvider(
-                      "https://9134d485-86e7-4fd7-afd5-28500eb1c97d.mock.pstmn.io"),
-                  listType: SupportedListItems.activities)),
-        }));
+        child: MaterialApp(
+            title: "DVI App",
+            debugShowCheckedModeBanner: false,
+//            home: LoginPage(userRepository: widget.userRepository),
+            routes: <String, WidgetBuilder>{
+              "/": (BuildContext context) {
+                return BlocBuilder<AuthenticationEvent, AuthenticationState>(
+                    bloc: _authenticationBloc,
+                    builder: (BuildContext context, AuthenticationState state) {
+                      if (state.isAuthenticated) {
+                        widget.defaultHome = HomePage();
+                      }
+                      return widget.defaultHome;
+                    });
+              },
+              "/movie": (BuildContext context) => new ListPage(
+                  title: "Movies",
+                  userRepository: widget.userRepository,
+                  listRepository: ListRepository(
+                      listApiProvider: MovieListApiProvider("http://api.themoviedb.org"),
+                      listType: SupportedListItems.movie)),
+              "/equipment": (BuildContext context) => new ListPage(
+                  title: "Equipments",
+                  userRepository: widget.userRepository,
+                  listRepository: ListRepository(
+                      listApiProvider: EquipmentListApiProvider(
+                          "https://9134d485-86e7-4fd7-afd5-28500eb1c97d.mock.pstmn.io"),
+                      listType: SupportedListItems.equipments)),
+              "/scan": (BuildContext context) => new ScanScreen(),
+              "/note": (BuildContext context) => new FormPage(),
+              "/activity": (BuildContext context) => new ListPage(
+                  title: "Activities",
+                  userRepository: widget.userRepository,
+                  listRepository: ListRepository(
+                      listApiProvider: ActivityListApiProvider(
+                          "https://9134d485-86e7-4fd7-afd5-28500eb1c97d.mock.pstmn.io"),
+                      listType: SupportedListItems.activities)),
+            }));
   }
 }
